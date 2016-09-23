537c871f4e4aa61b15e3df5b72d7a997f39c57e1
liuxuewen <457460089@qq.com>
2016-09-23 14:33:24 +0800

    css

--- demo/jd_wap/js/time.js
+++ demo/jd_wap/js/time.js
@@ -15,17 +15,37 @@ function Calender(options) {
         '1': '<li data-type="choose"><div><span>s%</span><div><strong>￥m%</strong><p>剩余：<em>l%</em></p></div></div></li>'
     };
     this.data = options.data || {
-        '2016': {
-            '9': {
-                '11': {
-                    last: '6',
-                    money: '2800'
-                }
-            }
+        "1": {
+            "status": false
+        },
+        "2": {
+            "status": true,
+                "last": 8,
+                "money": 5155
         }
     };
 
     this.fnInit();
+}
+Calender.prototype.getNowDate = function(){
+    var _this = this;
+    return {
+        year: _this.now.getFullYear(),
+        month: addZero(_this.now.getMonth()*1 + 1)
+    };
+    function addZero(n){
+        return n>=10? n: '0'+n;
+    }
+};
+Calender.prototype.getData = function (succ) {
+    var _this = this;
+    var _key = $("#travel_key").val();
+    var _id = $("#travel_id").val();
+    var _date = this.getNowDate();
+    $.post('/travel/tmpApi', {key: _key, travel: _id, date: _date.year+_date.month}, function (data) {
+        _this.data = data;
+        succ && succ();
+    }, 'json');
 }
 Calender.prototype.fnInit = function (){
     var _this = this;
@@ -44,24 +64,27 @@ Calender.prototype.fnInit = function (){
 Calender.prototype.fnShow = function () {
     this.$year.html(this.now.getFullYear());
     this.$month.html(this.now.getMonth() + 1);
-    var _arrLi = [];
-    for(var k = 0, _day = this.fnGetWeekDay(); k < _day; k++){
-        _arrLi.push(this.sLi[0].replace('s%', ''));
-    }
-    for(var i = 0, _lastDate = this.fnGetLastDate(); i < _lastDate; i++){
-        var _str = this.sLi[0].replace('s%', i + 1);
-        var _year = this.now.getFullYear(),
-            _month = this.now.getMonth() + 1,
-            _date = i + 1;
-        if(this.data[_year] && this.data[_year][_month] && this.data[_year][_month][_date]){
-            _str = this.sLi[1].replace('s%', i + 1)
-                    .replace('m%', this.data[_year][_month][_date].money)
-                    .replace('l%', this.data[_year][_month][_date].last);
+    var _this = this;
+    this.getData(function () {
+        var _arrLi = [];
+        for(var k = 0, _day = _this.fnGetWeekDay(); k < _day; k++){
+            _arrLi.push(_this.sLi[0].replace('s%', ''));
         }
-        _arrLi.push(_str);
+        for(var i = 0, _lastDate = _this.fnGetLastDate(); i < _lastDate; i++){
+            var _str = _this.sLi[0].replace('s%', i + 1);
+            var _year = _this.now.getFullYear(),
+                _month = _this.now.getMonth() + 1,
+                _date = i + 1;
+            if(_this.data[_date] && _this.data[_date].status == true){
+                _str = _this.sLi[1].replace('s%', i + 1)
+                    .replace('m%',  _this.data[_date].money)
+                    .replace('l%',  _this.data[_date].last);
+            }
+            _arrLi.push(_str);
 
-    }
-    this.$parent.html(_arrLi.join(''));
+        }
+        _this.$parent.html(_arrLi.join(''));
+    })
 };
 Calender.prototype.fnGetWeekDay = function () {
     this.now.setDate(1);
